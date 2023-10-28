94524691cd5ea877625a8a59354a5a1e3dd0d23b
DhanukaManish <mdhanuka1993@gmail.com>
2023-10-28 23:13:02 +0530

    Updated announcement

--- constants/Announcements.js
+++ constants/Announcements.js
@@ -1,5 +1,10 @@
 export const announcements = [
   ["Shop above ", "₹499", " to avail ", "free delivery."],
-  ["Order before ", "3:00 PM", " will be delivered by ", "evening."],
+  [
+    "Orders placed before ",
+    "3:00 PM",
+    " will be delivered on the ",
+    "same day.",
+  ],
   ["Orders placed after ", "3:00 PM", " will be delivered the ", "next day."],
 ];
